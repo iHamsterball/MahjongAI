72bf66dcb0e6f82c8aa8817bbfae876972e5f7d7
Cother <ihamsterball@gmail.com>
2021-12-15 15:01:56 +0800

    Add Cowboy.Sockets library

---
+++ SolutionVersion.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Cowboy.Sockets is a C# library for building sockets based services.")]
+[assembly: AssemblyCompany("Dennis Gao")]
+[assembly: AssemblyProduct("Cowboy.Sockets")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 Dennis Gao")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: ComVisible(false)]
